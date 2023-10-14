a4f5ba37ceec4e3b2f88efa9b038fb3879298139
gsverhoeven <gertjan.verhoeven@gmail.com>
2023-10-14 19:58:10 +0200

    add XFCE, xrdp and firefox

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+ 
+sudo apt install net-tools
+sudo apt-get update 
+sudo apt install -y inxi
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
+sudo apt install -y xfce4-session
 
-#sudo apt-get update && sudo apt install net-tools
-sudo apt-get update && sudo apt install -y inxi
+sudo apt-get -y install xrdp
+sudo systemctl enable xrdp
+sudo adduser xrdp ssl-cert
+echo xfce4-session >~/.xsession
+sudo service xrdp restart
+
+#sudo passwd azureuser
+
+sudo apt install firefox
