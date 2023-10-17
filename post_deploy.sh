4af22a00e11234cd36b8b9eb5e73d9a2a8c042db
gsverhoeven <gertjan.verhoeven@gmail.com>
2023-10-17 10:09:52 +0200

    apt-get is for scripts

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
  
 # BASIC TOOLING
-sudo apt update 
-sudo apt install -y net-tools inxi
+sudo apt-get update 
+sudo apt-get install -y net-tools inxi
 
 # XFCE & XRDP
-sudo DEBIAN_FRONTEND=noninteractive apt -y install xfce4
-sudo apt install -y xfce4-session
-sudo apt -y install xrdp
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
+sudo apt-get install -y xfce4-session
+sudo apt-get -y install xrdp
 sudo systemctl enable xrdp
 sudo adduser xrdp ssl-cert
 echo xfce4-session >~/.xsession
 sudo service xrdp restart
 
 # FIREFOX
-sudo apt install -y firefox
+sudo apt-get install -y firefox
 
 
 #sudo passwd azureuser
