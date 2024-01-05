32fbe2157cc7e944a3ce3f1832e87cca1ac90bc0
gsverhoeven <gertjan.verhoeven@gmail.com>
2024-01-05 14:16:23 +0100

    remove software virt as fallback and increase mem size

--- qemu/do_qemu.sh
+++ qemu/do_qemu.sh
@@ -16,9 +16,9 @@ gnome-terminal -- python3 -m http.server --directory .
 qemu-system-x86_64                                            \
     -net nic                                                    \
     -net user                                                   \
-    -machine accel=kvm:tcg                                      \
+    -machine accel=kvm                                        \
     -cpu host                                                   \
-    -m 1024                                                      \
+    -m 2048                                                      \
     -nographic                                                  \
     -hda jammy-server-cloudimg-amd64.img                        \
     -smbios type=1,serial=ds='nocloud;s=http://10.0.2.2:8000/'
